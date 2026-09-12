5330f39abf41d8c4fb449a4411a7f553e44d71f5
Hanumanthu2005 <hanumanthurao9381@gmail.com>
2026-09-12 12:50:09 +0530

    perf: optimize product queries and N + 1 query problem

--- src/main/java/com/hanu/AiEcommerce/product/controller/ProductController.java
+++ src/main/java/com/hanu/AiEcommerce/product/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 
 @RestController
@@ -63,4 +64,5 @@ public class ProductController {
                 productService.getAllProducts(pageable, request)
         );
     }
+
 }
--- src/main/java/com/hanu/AiEcommerce/product/repository/ProductRepository.java
+++ src/main/java/com/hanu/AiEcommerce/product/repository/ProductRepository.java
@@ -1,6 +1,10 @@
 package com.hanu.AiEcommerce.product.repository;
 
 import com.hanu.AiEcommerce.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -15,4 +19,11 @@ public interface ProductRepository
     Optional<Product> findBySku(String sku);
 
     boolean existsBySku(String sku);
+
+    @Override
+    @EntityGraph(attributePaths = {"category"})
+    Page<Product> findAll(
+            Specification specification,
+            Pageable pageable
+    );
 }
